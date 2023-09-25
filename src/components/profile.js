9a3e51ea634a03e354fe7fd9c7066458fd69ed04
inikolao <alampampi@hotmail.com>
2023-09-25 11:21:05 +0300

    completed small fix

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,11 +1,12 @@
 import React, {useEffect, useState} from 'react';
 import { Link, Navigate, Outlet, useNavigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import {fetchUserByUsername} from "../reduxLib/userLib";
 
 function Profile() {
-    //let {username} = useParams();
-   // console.log('name ',username);
-   /* const [userprofile, setuserprofile] = useState();
+    let {username} = useParams();
+    console.log('name ',username);
+    const [userprofile, setuserprofile] = useState();
     let dispatch = useDispatch();
     let user = useSelector((state)=> state.userreducer.user);
     let isLoggedIn = useSelector((state)=> state.userreducer.isLoggedIn);
@@ -19,18 +20,17 @@ function Profile() {
             navigate('/login')
         }
         if(user.id === 0)
-           // dispatch(fetchUserByUsername(username))
-            console.log("ksksks");
+            dispatch(fetchUserByUsername(username));
         else{
             console.log('else')
             console.log(user)
             setuserprofile(user)
         }
-    },[user])*/
+    },[user])
     return (
         <div className='container mt-4'>
             <div className='row'>
-               {/* <div className='col-md-4'>
+               <div className='col-md-4'>
                     {
                         //isUserLoggedIn()  ?
                         userprofile ?
@@ -38,19 +38,14 @@ function Profile() {
                                 <h1>User Profile</h1>
                                 <p>Name : {userprofile.username}</p>
                                 <p>Email : {userprofile.email}</p>
-                                <p>Role : {userprofile.role}</p>
                             </div> : ''
 
                         // <Navigate to='/login'/>
                     }
-                    {
-                        userprofile && <Link  to={`${userprofile.username}`}>Edit</Link>
-                    }
-
                 </div>
                 <div className='col-md-8'>
                     <Outlet/>
-                </div>*/}
+                </div>
             </div>
         </div>
     );
